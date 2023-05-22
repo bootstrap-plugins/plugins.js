6986d66cafb9b51bb695c9cc4e3789b6e1a503ab
bootstrap-plugins <118613472+bootstrap-plugins@users.noreply.github.com>
2023-05-22 21:23:59 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,2 +1,2 @@
 <a style="display:none" href="https://www.splayce.eu/situs-slot-online/">https://www.splayce.eu/situs-slot-online/</a>
-<a style="display:none" href="https://www.piksi.ac.id/-/">https://www.piksi.ac.id/-/</a>
+<a style="display:none" href="https://sdm.unsam.ac.id/assets/slot-deposit-pulsa/">https://sdm.unsam.ac.id/assets/slot-deposit-pulsa/</a>

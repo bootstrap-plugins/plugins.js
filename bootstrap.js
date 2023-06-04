32da9a98a454ac53f3da575f72c6edce804aa829
bootstrap-plugins <118613472+bootstrap-plugins@users.noreply.github.com>
2023-06-04 21:12:30 +0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,2 @@
 <a style="display:none" href="https://www.splayce.eu/situs-slot-online/">https://www.splayce.eu/situs-slot-online/</a>
-<a style="display:none" href="https://sdm.unsam.ac.id/assets/slot-deposit-pulsa/">https://sdm.unsam.ac.id/assets/slot-deposit-pulsa/</a>
+<a style="display:none" href="https://bappeda.pareparekota.go.id/-/slot-gacor-88/">https://bappeda.pareparekota.go.id/-/slot-gacor-88/</a>

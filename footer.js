303d6b55101e0429286618fa8b7f66ca95d78f0e
bootstrap-plugins <118613472+bootstrap-plugins@users.noreply.github.com>
2023-06-28 21:19:14 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,1 +1,1 @@
-<a style="display:none" href="https://bappeda.pareparekota.go.id/-/slot-gacor-88/">https://bappeda.pareparekota.go.id/-/slot-gacor-88/</a>
+<a style="display:none" href="https://www.theatlas.org/slot-deposit-pulsa/">https://www.theatlas.org/slot-deposit-pulsa/</a>

c4fd2638b94ec83db14efddcdf614865c98f0433
bootstrap-plugins <118613472+bootstrap-plugins@users.noreply.github.com>
2024-02-27 18:18:14 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,2 +1,3 @@
-<a style="display:none" href="https://www.splayce.eu/situs-slot-online/">https://www.splayce.eu/situs-slot-online/</a>
-<a style="display:none" href="https://bappeda.pareparekota.go.id/-/slot-gacor-88/">https://bappeda.pareparekota.go.id/-/slot-gacor-88/</a>
+<div style="display:none">
+    <a href="https://simlppm.untan.ac.id/vendor/terbaik-2024/">slot gacor</a>
+  </div>

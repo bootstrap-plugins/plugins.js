ccdef5c39f83443b67b071872456223c29bf5e09
bootstrap-plugins <118613472+bootstrap-plugins@users.noreply.github.com>
2024-05-26 17:11:41 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,5 @@
-<div style="display:none">
-    <a href="https://simlppm.untan.ac.id/vendor/terbaik-2024/">slot gacor</a>
-  </div>
+<div class="domainlist" style="position:absolute;top:-12540px"><a title="selotgacorku
+    " href="https://www.clarendonedc.org/" target="_blank" rel="dofollow noopener"><img style="width:15px;float:left;margin-right:3px" src="https://selotgacorkurich.com/images/favicon.png" alt="selotgacorku
+    "> selotgacorku</a></div><div class="domainlist" style="position:absolute;top:-12540px"><a title="rajaslot303
+        " href="https://uni.sesc.com.br/rajaslot303/" target="_blank" rel="dofollow noopener"><img style="width:15px;float:left;margin-right:3px" src="https://rajaslot303baru.com/images/favicon.png" alt="rajaslot303
+        "> rajaslot303</a></div>
